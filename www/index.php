66e13b3801fe0e607b8fce2dd82f07bb48d673fa
Trautenberk <76706207+adamak362@users.noreply.github.com>
2023-03-14 19:39:42 +0100

    work on hero

--- www/index.php
+++ www/index.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
+
 $configurator = App\Bootstrap::boot();
 $container = $configurator->createContainer();
 $application = $container->getByType(Nette\Application\Application::class);
