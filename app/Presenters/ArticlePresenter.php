2b2c9c6bd6e94d8126782a80d3ce6d78f8e01f33
Trautenberk <76706207+adamak362@users.noreply.github.com>
2023-05-10 11:08:26 +0200

    add wysiwyg & articleForm

--- app/Presenters/ArticlePresenter.php
+++ app/Presenters/ArticlePresenter.php
@@ -22,10 +22,31 @@ final class ArticlePresenter extends BasePresenter
         }
 
     }
-    public function renderDefault(){
+    public function renderDefault()
+    {
     }
 
-    public function renderMain(){
+    public function renderAdd()
+    {
     }
 
+    protected function createComponentArticleForm()
+    {
+//        $caption = $this->record ? 'Upravit článek':'Vytvořit článek';
+        $form = new Form;
+        $form->addText('title','Titulek',)
+            ->setRequired();
+        $form->addText('perex','Perex')
+            ->setRequired();
+        $form->addTextArea('content','Obsah')
+            ->setRequired();
+        $form->addText('date','Datum publikace')
+            ->setHtmlType('date')
+            ->setRequired();
+
+        $form->addSubmit('send','Vytvořit');
+        $form->onSuccess[] = [$this,'articleFormSuccess'];
+
+        return $form;
+    }
 }
