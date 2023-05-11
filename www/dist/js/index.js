87cdc4d878f6849e04cff5009049710732b8f9a0
Trautenberk <76706207+adamak362@users.noreply.github.com>
2023-05-11 13:20:16 +0200

    add hamburger menu for phone in admin

--- www/dist/js/index.js
+++ www/dist/js/index.js
@@ -1,15 +1,8 @@
-// import {initAll} from "./initializers.js";
-//
-// document.addEventListener(`DOMContentLoaded`, () => {
-//     insertNetteFormsScript()
-//     initAll().then(() => {
-//         console.log("All modules successfully loaded")
-//     })
-// })
-//
-// function insertNetteFormsScript() {
-//
-//     const script = document.createElement("script")
-//     script.src = "https://nette.github.io/resources/js/3/netteForms.min.js"
-//     document.head.appendChild(script)
-// }
+import {initAll} from "./initializers.js";
+
+document.addEventListener(`DOMContentLoaded`, () => {
+    initAll().then(() => {
+        console.log("All modules successfully loaded")
+    })
+})
+
--- www/dist/js/initializers.js
+++ www/dist/js/initializers.js
@@ -2,80 +2,130 @@ import "./ckeditor/ckeditor.js"
 
 let editor;
 
-ClassicEditor
-    .create(document.querySelector('.js-wysiwyg'),{
-        toolbar: {
-            items: [
-                "heading",
-                "|",
-                "bold",
-                "italic",
-                "superscript",
-                "subscript",
-                "link",
-                "bulletedList",
-                "numberedList",
-                "removeFormat",
-                "code",
-                "|",
-                "indent",
-                "outdent",
-                "|",
-                "imageUpload",
-                "blockQuote",
-                "insertTable",
-                "mediaEmbed",
-                "undo",
-                "redo",
-            ],
-        },
-            heading: {
-                options: [
-                    {
-                        model: "paragraph",
-                        view: "p",
-                        title: "Paragraph",
-                        class: "ck-heading_paragraph",
-                    },
-                    {
-                        model: "heading1",
-                        view: "h3",
-                        title: "Heading 1",
-                        class: "ck-heading_heading1",
-                    },
-                    {
-                        model: "heading2",
-                        view: "h4",
-                        title: "Heading 2",
-                        class: "ck-heading_heading2",
-                    },
-                    {
-                        model: "heading3",
-                        view: {
-                            name: "h5",
-                            classes: "label",
+export function initCKEditor() {
+    // use observer to reduce load on JS engine
+    // eg: many instances are used in form multipliers
+    window.CKObserver =
+        window.CKObserver ||
+        new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    window.ClassicEditor.create(entry.target, {
+                        height: 400,
+                        simpleUpload: {
+                            uploadUrl: entry.target.dataset.uploadUrl,
+                            withCredentials: true,
+                            headers: {},
                         },
-                        title: "Label",
-                        class: "ck-heading_heading3",
-                    },
-                    {
-                        model: "heading4",
-                        view: {
-                            name: "span",
-                            classes: "btn-wysiwyg",
+                        toolbar: {
+                            items: [
+                                "heading",
+                                "|",
+                                "bold",
+                                "italic",
+                                "superscript",
+                                "subscript",
+                                "link",
+                                "bulletedList",
+                                "numberedList",
+                                "removeFormat",
+                                "code",
+                                "|",
+                                "indent",
+                                "outdent",
+                                "|",
+                                "imageUpload",
+                                "blockQuote",
+                                "insertTable",
+                                "mediaEmbed",
+                                "undo",
+                                "redo",
+                            ],
                         },
-                        title: "Tlačítko",
-                        class: "ck-heading_heading4",
-                    },
-                ],
-            },
+                        language: "cs",
+                        image: {
+                            toolbar: [
+                                "imageTextAlternative",
+                                "imageStyle:full",
+                                "imageStyle:side",
+                            ],
+                        },
+                        table: {
+                            contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
+                        },
+                        mediaEmbed: {
+                            previewsInData: true,
+                        },
+                        heading: {
+                            options: [
+                                {
+                                    model: "paragraph",
+                                    view: "p",
+                                    title: "Paragraph",
+                                    class: "ck-heading_paragraph",
+                                },
+                                {
+                                    model: "heading1",
+                                    view: "h3",
+                                    title: "Heading 1",
+                                    class: "ck-heading_heading1",
+                                },
+                                {
+                                    model: "heading2",
+                                    view: "h4",
+                                    title: "Heading 2",
+                                    class: "ck-heading_heading2",
+                                },
+                                {
+                                    model: "heading3",
+                                    view: {
+                                        name: "h5",
+                                        classes: "label",
+                                    },
+                                    title: "Label",
+                                    class: "ck-heading_heading3",
+                                },
+                                {
+                                    model: "heading4",
+                                    view: {
+                                        name: "span",
+                                        classes: "btn-wysiwyg",
+                                    },
+                                    title: "Tlačítko",
+                                    class: "ck-heading_heading4",
+                                },
+                            ],
+                        },
+                    })
+                        .then((editor) => {
+                            editor.model.document.on("change:data", () => {
+                                editor.updateSourceElement()
+                            })
+                        })
+                        .catch((err) => console.error(err.stack))
+                    window.CKObserver.unobserve(entry.target)
+                }
+            })
         })
-    .then(newEditor => {
-        editor = newEditor;
+    window.CKObserver.disconnect()
+    document.querySelectorAll(".js-wysiwyg").forEach((field) => {
+        window.CKObserver.observe(field)
     })
-    .catch( error => console.log(error))
+}
 
-document.querySelector( '#send' ).addEventListener( 'click', () => {
-    const editorData = editor.getData();
-    console.log(editorData)
-} );
+function initHamburgerMenu()
+{
+    var burgerMenu = document.getElementById('burger-menu');
+    var overlay = document.getElementById('menu');
+    burgerMenu.addEventListener('click',function(){
+        this.classList.toggle("close");
+        overlay.classList.toggle("overlay");
+    });
+}
+
+
+
+export async function initAll() {
+    initCKEditor()
+    initHamburgerMenu()
+}
