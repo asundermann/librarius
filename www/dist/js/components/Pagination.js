0eb7848ffe09357b8829b197c5f30023c11f6074
asundermann <sundermann.adam@gmail.com>
2023-06-28 16:58:07 +0200

    quickfix

--- www/dist/js/components/Pagination.js
+++ www/dist/js/components/Pagination.js
@@ -11,5 +11,7 @@ if (page == null){
     pageOne.classList.remove("text-gray-500")
     pageOne.classList.add("text-librarius-900")
 }
-concretePage.classList.remove("text-gray-500")
-concretePage.classList.add("text-librarius-900")
+else{
+    concretePage.classList.remove("text-gray-500")
+    concretePage.classList.add("text-librarius-900")
+}
